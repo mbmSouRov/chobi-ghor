68c65244742f77df8cee2e62611304dc5fb0b9a3
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-11-08 11:31:37 +0600

    all packages install

---
+++ src/Components/Routes/Routes.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import Home from "../Home/Home";
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default Routes;
---
+++ src/Components/SharedPages/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <p>This is footer Page</p>
+    </div>
+  );
+};
+
+export default Footer;
