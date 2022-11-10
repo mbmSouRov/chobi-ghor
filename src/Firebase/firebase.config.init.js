d351a41ef39dcc037c8a67e9eb1072c62a316fa1
Murshid Bin Masud Sourov <61899629+mbmSouRov@users.noreply.github.com>
2022-11-10 17:38:47 +0600

    firebase env added

--- src/Firebase/firebase.config.init.js
+++ src/Firebase/firebase.config.init.js
@@ -5,18 +5,18 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_apiKey,
-  // authDomain: process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId,
-  apiKey: "AIzaSyAU3Ln2sF6P1Mc49MSH-aHi-I_qqSbPjuY",
-  authDomain: "chobi-ghor.firebaseapp.com",
-  projectId: "chobi-ghor",
-  storageBucket: "chobi-ghor.appspot.com",
-  messagingSenderId: "291073138155",
-  appId: "1:291073138155:web:79900ae233f8608e814dae",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  // apiKey: "AIzaSyAU3Ln2sF6P1Mc49MSH-aHi-I_qqSbPjuY",
+  // authDomain: "chobi-ghor.firebaseapp.com",
+  // projectId: "chobi-ghor",
+  // storageBucket: "chobi-ghor.appspot.com",
+  // messagingSenderId: "291073138155",
+  // appId: "1:291073138155:web:79900ae233f8608e814dae",
 };
 
 // Initialize Firebase
